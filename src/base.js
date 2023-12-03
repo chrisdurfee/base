381e52e94b0bc8b052aca7e88614638623d37c6a
tech_e <chris.durfee@dentalqore.com>
2023-12-03 02:11:15 -0700

    updated base.

--- src/base.js
+++ src/base.js
@@ -2,19 +2,21 @@ import { base } from './main/base.js';
 import { Ajax } from './modules/ajax/ajax.js';
 import { Atom } from './modules/atom/atom.js';
 import { Component } from './modules/component/component.js';
-import { Unit } from './modules/component/unit.js';
 import { Jot } from './modules/component/jot.js';
+import { Unit } from './modules/component/unit.js';
 import { dataBinder } from './modules/data-binder/data-binder.js';
 import { Data, Model, SimpleData } from './modules/data/data.js';
-import { Directives } from './modules/layout/directives/directives.js';
-import {} from './modules/layout/directives/core/default-directives.js';
+import { Html } from './modules/html/html.js';
 import { Builder } from './modules/layout/builder.js';
+import { } from './modules/layout/directives/core/default-directives.js';
+import { Directives } from './modules/layout/directives/directives.js';
 import { NavLink, router } from './modules/router/router.js';
 import { StateTracker as State } from './modules/state/state-tracker.js';
 
 base.augment(
 {
     Ajax,
+    Html,
     dataBinder,
     Data,
     SimpleData,
@@ -26,6 +28,6 @@ base.augment(
 });
 
 export {
-    Ajax, Atom, Directives, Builder, Component, Data, Jot, Model, NavLink, SimpleData, State, Unit, base, dataBinder, router
+    Ajax, Atom, Builder, Component, Data, Directives, Html, Jot, Model, NavLink, SimpleData, State, Unit, base, dataBinder, router
 };
 
