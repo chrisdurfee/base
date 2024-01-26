107e50d6926f84e8746146e6c549e3b139415d92
tech_e <chris.durfee@dentalqore.com>
2024-01-26 00:20:52 -0700

    Update package.json and base.js version numbers

--- src/main/base.js
+++ src/main/base.js
@@ -1,6 +1,6 @@
 /**
  * Base Framework
- * @version 3.0.10
+ * @version 3.0.26
  * @author Chris Durfee
  * @file This is a javascript framework to allow complex
  * functions to work in many browsers and versions.
@@ -32,7 +32,7 @@ class Base
 		/**
 		 * @member {string} version
 		 */
-		this.version = '3.0.10';
+		this.version = '3.0.26';
 
 		/**
 		 * @member {array} errors
