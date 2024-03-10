cbf25db89598ae18059158f971da9b69b5e7860c
tech_e <chris.durfee@dentalqore.com>
2024-03-10 14:40:41 -0600

    Add TypeScript support and configuration files

---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+
+export default defineConfig({
+    plugins: [
+        dts({
+            outDir: '/dist/',
+        })
+    ]
+});
