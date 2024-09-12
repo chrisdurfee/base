badf2a88d5dc8d1e9c52656054a4957a516bbbcf
tech_e <chris.durfee@dentalqore.com>
2024-09-12 16:40:10 -0600

    Refactor SimpleData class to remove unused code and unnecessary imports

--- src/modules/data/types/simple-data.js
+++ src/modules/data/types/simple-data.js
@@ -1,5 +1,4 @@
-import { dataBinder } from '../../data-binder/data-binder.js';
-import { BasicData, createEventMessage } from './basic-data.js';
+import { BasicData } from './basic-data.js';
 
 /**
  * SimpleData
@@ -12,26 +11,5 @@ import { BasicData, createEventMessage } from './basic-data.js';
  */
 export class SimpleData extends BasicData
 {
-	/**
-	 * This will publish an update to the data binder.
-	 *
-	 * @protected
-	 * @param {string} attr
-	 * @param {*} val
-	 * @param {*} committer
-	 * @param {string} event
-	 * @returns {void}
-	 */
-	_publish(attr, val, committer, event)
-	{
-		const message = createEventMessage(attr, event);
-		this.eventSub.publish(message, val, committer);
 
-		/**
-		 * This will set the committer to the current object if it is not set.
-		 * This is deferred until the local event is published.
-		 */
-		committer = committer || this;
-		dataBinder.publish(this._dataId + attr, val, committer);
-	}
 }
