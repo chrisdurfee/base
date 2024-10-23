88bc68e996659490dbd41a49ad5680886a9e61b7
tech_e <chris.durfee@dentalqore.com>
2024-10-23 16:54:36 -0600

    Enhance useParent function to parse attributes and set directives in parent element

--- src/modules/layout/directives/core/parent/parent-directives.js
+++ src/modules/layout/directives/core/parent/parent-directives.js
@@ -1,3 +1,7 @@
+import { Builder } from "../../../builder.js";
+import { Parser } from "../../../element/parser.js";
+import { HtmlHelper } from "../../../html-helper.js";
+
 /**
  * This will cache the element in the parent.
  *
@@ -31,7 +35,15 @@ export const useParent = (ele, callBack, parent) =>
         return;
     }
 
-    callBack(parent, ele);
+    const attributes = callBack(parent, ele);
+    if (!attributes)
+    {
+        return;
+    }
+
+    const parsed = Parser.parse(attributes, parent);
+    HtmlHelper.addAttributes(ele, parsed.attr, parent);
+    Builder.setDirectives(ele, parsed.directives, parent);
 };
 
 /**
