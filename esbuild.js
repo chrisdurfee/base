1a06e7a44427c4935e3c21093138774b3358e181
tech_e <chris.durfee@dentalqore.com>
2025-01-17 14:06:56 -0700

    Format esbuild.js for consistent indentation

--- esbuild.js
+++ esbuild.js
@@ -1,14 +1,14 @@
 import { build } from 'esbuild';
 
 build({
-    entryPoints: ['src/base.js'],
-    outdir: 'dist',
-    bundle: true,
-    sourcemap: true,
-    minify: true,
-    splitting: true,
-    treeShaking: true,
-    format: 'esm',
-    target: ['esnext'],
+	entryPoints: ['src/base.js'],
+	outdir: 'dist',
+	bundle: true,
+	sourcemap: true,
+	minify: true,
+	splitting: true,
+	treeShaking: true,
+	format: 'esm',
+	target: ['esnext'],
 })
 .catch(() => process.exit(1));
