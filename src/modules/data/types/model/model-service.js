738c0208ce7b9f30e4d9ce7189959de0e4860948
tech_e <chris.durfee@dentalqore.com>
2025-04-10 13:58:31 -0600

    update

--- src/modules/data/types/model/model-service.js
+++ src/modules/data/types/model/model-service.js
@@ -300,7 +300,7 @@ export class ModelService
 	delete(instanceParams, callBack)
 	{
 		const id = this.model.get('id');
-		const params = (typeof id !== 'undefined')? 'id=' + id : this.setupObjectData();;
+		const params = (typeof id !== 'undefined')? 'id=' + id : this.setupObjectData();
 
 		return this._delete('', params, instanceParams, callBack);
 	}
