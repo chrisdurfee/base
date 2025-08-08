aa897fb031bc3a95ba70ce30a22c81b0c0dfb01e
tech_e <chris.durfee@dentalqore.com>
2025-08-08 16:23:14 -0600

    update version to 3.0.465; modify lastCursor handling in ModelService to prevent undefined/null values

--- src/modules/data/types/model/model-service.js
+++ src/modules/data/types/model/model-service.js
@@ -360,7 +360,7 @@ export class ModelService
 			'&orderBy=' + orderBy +
 			'&groupBy=' + groupBy +
 			'&search=' + search +
-			'&lastCursor=' + lastCursor;
+			'&lastCursor=' + (lastCursor === 'undefined' || lastCursor === null) ? '' : lastCursor;
 
 		return this._get('', params, instanceParams, callBack);
 	}
