073713a9d4520b2d99e83e0ee4c085736ecdb8ca
tech_e <chris.durfee@dentalqore.com>
2026-03-05 17:08:17 -0700

    fix: add reference path for data types in data.js

--- src/modules/data/data.js
+++ src/modules/data/data.js
@@ -1,3 +1,4 @@
+/// <reference path="./types/data-types.d.ts" />
 export { Data } from './types/deep-data/deep-data.js';
 export { Model } from './types/model/model.js';
 export { SimpleData } from './types/simple-data.js';
